9096d584a05e741019ba2206bad66fc9b63d1159
Thomas <thomasconnerse@gmail.com>
2023-01-18 11:39:30 -0600

    Update ElementaryCellularAutomaton.java

--- ElementaryCellularAutomaton.java
+++ ElementaryCellularAutomaton.java
@@ -25,21 +25,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        int rule = -1;
+        //Probably not the best way to perform rudimentary input validation, but input validation
+        //was sort of an after-thought with this program
         
+        int rule = -1;
         while(rule < 0 || rule > 255){
             System.out.print("Enter rule (0-255): ");
             rule = scan.nextInt();
         }
         
         int numGens = -1;
-        
         while(numGens<=0){
             System.out.print("Enter num generations: ");
             numGens = scan.nextInt();
         }
         
-        int cur = 1<<16; //This makes it easier to change starting pos intead of putting an int literal
+        int cur = 1<<16;    // This makes it easier to change starting pos intead of putting an int literal
+                            // 16 initializes the bit in the strting row to the center of the row
         
         for(int i = 0; i <= numGens; i++){
             drawRow(cur);
@@ -48,22 +50,21 @@ public class Main {
         scan.close();
     }
     
+    
+    //Calculates the binary string representing the next row in the cellular automaton
+    
     public static int calcNextRow(int prev, int rule){
         int cur = 0;
         for(int i = 0; i < 32; i++){
-            for(int j = 0; j < 8; j++){
-                // ^^^ Remove this second loop
-                if((j^(prev&7))==0){
-                    if((1&(rule>>j))!=0)
-                        cur++;
-                    break;
-                }
-            }
+            if((rule&(1<<(prev&7)))!=0)
+                cur++;
             cur=cur<<1;
             prev=prev>>1;
         }
         return cur>>1;
     }
+    
+    //Draws the given bit string IS ascii characters
     
     public static void drawRow(int row){
         for(int i = 0; i < 32; i++){
